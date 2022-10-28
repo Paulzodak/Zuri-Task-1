c33ef173246112da807c5550ce13889ef8c0480a
Zodak <ojepaul4jesus@gmail.com>
2022-10-28 12:41:58 +0100

    started mobile design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import DesktopApp from "./DesktopApp";
+import { useMediaQuery } from "react-responsive";
+import MobileApp from "./MobileApp";
 
 function App() {
-  return (
-    <>
-      <DesktopApp />
-    </>
-  );
+  const mobile = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
+  return <>{mobile ? <MobileApp /> : <DesktopApp />}</>;
 }
 
 export default App;
---
+++ src/MobileApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import profileImage from "./images/my_profile_img.jpg";
+import LinkItem from "./components/LinkItem";
+import zuriIcon from "./images/zuri.svg";
+import Iicon from "./images/I4G.svg";
+import slack_logo from "./images/slack.svg";
+import github_logo from "./images/github.svg";
+import linkDesktop from "./images/linkDesktop.svg";
+
+const MobileApp = () => {
+  return <div></div>;
+};
+
+export default MobileApp;
