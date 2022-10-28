ae7dfe993a0f6063b9c7dc1cc5f8bd49af89e1a0
Zodak <ojepaul4jesus@gmail.com>
2022-10-28 11:42:25 +0100

    added footer section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import profileImage from "./images/profile_img.svg";
 import LinkItem from "./components/LinkItem";
+import zuriIcon from "./images/zuri.svg";
+import Iicon from "./images/I4G.svg";
+import slack_logo from "./images/slack.svg";
+import github_logo from "./images/github.svg";
 
 function App() {
   const links = [
@@ -40,10 +44,23 @@ function App() {
         <img id={"profile_img"} src={profileImage} alt={"Profile_Picture"} />
         <h1 id={"twitter"}>OjePaul</h1>
         <h1 id={"slack"}> Oje Paul</h1>
+      </section>
+      <section>
         {links.map((item) => (
           <LinkItem item={item} />
         ))}
       </section>
+
+      <section id={"socials"}>
+        <img className="logo" src={slack_logo} alt={"slack_logo"}></img>
+        <img className="logo" src={github_logo} alt={"github_logo"}></img>
+      </section>
+      <hr className="hr" />
+      <footer id="footer">
+        <img className="zuri" src={zuriIcon} alt={"zuriIcon"}></img>
+        <div className="hng"> HNG Internship 9 Frontend Task</div>
+        <img className="ingressive" src={Iicon} alt={"14Gicon"}></img>
+      </footer>
     </div>
   );
 }
